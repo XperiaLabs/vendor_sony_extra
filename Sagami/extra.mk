b9b69419166153418d9b66bad9e05f864908d2ef
saku-bruh <notwolfyy@gmail.com>
2023-11-28 21:49:35 +0200

    extra: audio: Drop DTS:X and DIRAC
    * DIRAC doesn't even work without a prebuilt Xiaomi audio stack so it's useless and DTS:X has no use now since Dolby works on UDC after some FWB patches
    
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- Sagami/extra.mk
+++ Sagami/extra.mk
@@ -46,9 +46,7 @@ endif
 
 # Sound Enhancements
 ifeq ($(TARGET_SHIPS_SOUND_ENHANCEMENT),true)
-    $(call inherit-product, vendor/sony/extra/Sagami/audio/dtsx/ultra.mk)
-#    $(call inherit-product, vendor/sony/extra/Sagami/audio/dolby/dolby.mk)
-#    $(call inherit-product, vendor/sony/extra/Sagami/audio/dirac/dirac.mk)
+    $(call inherit-product, vendor/sony/extra/Sagami/audio/dolby/dolby.mk)
 endif
 
 # Game Controllers
