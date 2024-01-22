0a03be869ffed16ebad637e480081a7a5e280cd6
saku-bruh <saku-bruh@proton.me>
2024-01-22 04:37:58 +0200

    extra: Add support for the Tama Series
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

---
+++ Tama/Android.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter akari akari_kddi apollo apollo_kddi akatsuki akatsuki_kddi aurora aurora_kddi,$(TARGET_DEVICE)),)
+
+endif
---
+++ Tama/extra.mk
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2023 XperiaLabs Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Soong Namespace
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/sony/extra/Tama
+
+# Flags
+TARGET_SHIPS_SONY_FRAMEWORK ?= true
+TARGET_SHIPS_APERTURE_CAMERA ?= true
+TARGET_SHIPS_SONY_APPS ?= true
+TARGET_SHIPS_SOUND_ENHANCEMENT ?= true
+TARGET_SUPPORTS_GAME_CONTROLLERS ?= true
+
+# Sony Framework
+ifeq ($(TARGET_SHIPS_SONY_FRAMEWORK),true)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/sony/extra/Common/framework/system/,$(TARGET_COPY_OUT_SYSTEM)/) \
+    $(call find-copy-subdir-files,*,vendor/sony/extra/Common/framework/system_ext/,$(TARGET_COPY_OUT_SYSTEM_EXT)/) \
+    $(call find-copy-subdir-files,*,vendor/sony/extra/Common/framework/product/,$(TARGET_COPY_OUT_PRODUCT)/)
+endif
+
+# Sony Camera
+ifeq ($(TARGET_SHIPS_APERTURE_CAMERA),true)
+    $(call inherit-product, vendor/sony/extra/Tama/camera/camera.mk)
+endif
+
+
+# Sony Apps
+ifeq ($(TARGET_SHIPS_SONY_APPS),true)
+    $(call inherit-product, vendor/sony/extra/Tama/apps/apps.mk)
+endif
+
+# Sound Enhancements
+ifeq ($(TARGET_SHIPS_SOUND_ENHANCEMENT),true)
+    $(call inherit-product, vendor/sony/extra/Tama/audio/dolby/dolby.mk)
+endif
+
+# Game Controllers
+ifeq ($(TARGET_SUPPORTS_GAME_CONTROLLERS),true)
+    $(call inherit-product, vendor/sony/extra/Common/controllers/gc.mk)
+endif
